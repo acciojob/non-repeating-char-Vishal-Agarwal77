913eeaa9100710fff2fb749f296885613e4dc5c9
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-19 02:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let temp=[];
+	for(let i=0;i<str.length;i++){
+		if(temp[str.charCodeAt(i)]>=0){
+			temp[str.charCodeAt(i)]++;
+		}
+		else{
+			temp[str.charCodeAt(i)]=1;
+		}
+	}
+	for(let i=0;i<temp.length;i++){
+		if(temp[i]===1){
+			return String.fromCharCode(i);
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
